c7d46b5d7c5074d50bb1109ba4bfd60ac4bdbdbb
Mohamed Yansane <yansane472@gmail.com>
2023-04-15 14:41:43 +0000

    adding components child folders path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: [
         './public/*.{js,jsx,ts,tsx}',
         './components/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
