293b4d27fa76b7b2987ec051a38403df91dbbe2a
Mohamed Yansane <yansane472@gmail.com>
2023-04-15 14:39:05 +0000

    creating folders for my diffierents components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <>
+      <section className="header w-full">
+        <h1 className="text-white">Hi it's header</h1>
+      </section>
+    </>
+  );
+};
